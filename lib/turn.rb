291c008cb0a159235a36e158a991679134c12afa
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-04 11:32:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,3 +22,16 @@ end
 def position_taken?(board, answer)
   board[answer] != " " 
 end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    answer = gets.chomp
+    answer = input_to_index(answer)
+    if valid_move?(answer, board)
+      move(board, answer)
+      puts display_board(board)
+    else
+      puts "That is an invalid entry!"
+      turn(board)
+    end 
+end
